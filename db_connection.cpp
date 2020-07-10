2e33484bb9873eba613f06add203c3f67b507eb2
talorno <giova@Shiva.localdomain>
2020-07-10 12:36:24 +0200

    a

--- db_connection.cpp
+++ db_connection.cpp
@@ -4,22 +4,21 @@
 
 db_connection::db_connection(QString hostname, QString dbname, QString username,  QString password){
 
-QSqlDatabase dbCon_named = QSqlDatabase::addDatabase("QMYSQL");
-    dbCon_named.setHostName(hostname);
-    dbCon_named.setDatabaseName("TrainingPlanV2");
-    dbCon_named.setUserName("archery_manager");
-    dbCon_named.setPassword("archery");
+    dbCon = QSqlDatabase::addDatabase("QMYSQL");
+    dbCon.setHostName(hostname);
+    dbCon.setDatabaseName("TrainingPlanV2");
+    dbCon.setUserName("archery_manager");
+    dbCon.setPassword("archery");
     //dbConState = dbCon.open();
     }
 
 db_connection::db_connection(){
 
-    QSqlDatabase dbCon = QSqlDatabase::addDatabase("QMYSQL");
+    dbCon = QSqlDatabase::addDatabase("QMYSQL");
     dbCon.setHostName("localhost");
     dbCon.setDatabaseName("TrainingPlanV2");
     dbCon.setUserName("archery_manager");
     dbCon.setPassword("archery");
-    qDebug() << QSqlDatabase::drivers();
 
     if(dbCon.open()){qDebug()<<"Database opened! - FROM DBCONNECTION CLASS";}
     else{qDebug() << dbCon.lastError().text();}
